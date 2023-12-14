a58db82965c9d8a0e485a7a922ed09d1e68d1cb4
James <james.ekasiba@student.moringaschool.com>
2023-12-14 20:25:58 +0300

    add Delete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
 from database import get_db
 from models import Review
@@ -11,15 +11,30 @@ def reviews(db: Session = Depends(get_db)):
   reviews= db.query(Review).all()
   return reviews
 
+@app.get('/reviews/{review_id}')
+def review(review_id: int, db: Session = Depends(get_db)):
+  review = db.query(Review).filter(Review.id == review_id).first()
+  return review
+
 @app.post('/reviews')
-def create(review: ReviewSchema):
-  print(review)
-  return {'message':'Review created successfully'}
+def create(review: ReviewSchema, db: Session = Depends(get_db)):
+  new_review = Review(**review.model_dump())
+  db.add(new_review)
+  db.commit()
+  db.refresh(new_review)
+  return {'message':'Review created successfully', 'review': new_review}
 
 @app.patch('/reviews/{reviews_id}')
 def updated_review(reviews_id: int):
   return {'message':f'Review {reviews_id} created successfully'}
 
 @app.delete('/reviews/{reviews_id}')
-def delete_reviews(reviews_id: int):
-  return {'message':f'Review {reviews_id} deleted successfully'}
+def delete_reviews(reviews_id: int, db: Session = Depends(get_db)):
+  delete_reviews = db.query(Review).filter(Review.id == reviews_id).first()
+
+  if delete_reviews == None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Review {reviews_id} does not exist")
+  else:
+     delete_reviews.delete()
+     db.commit()
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
