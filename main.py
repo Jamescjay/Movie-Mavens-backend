91d5ec1187037eaa0d8f20383e2688070056339d
James <james.ekasiba@student.moringaschool.com>
2023-12-14 17:15:06 +0300

    setup Reviews routes

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/reviews')
 def index():
-  return {'message': 'Welcome to my first api'}
+  return []
+
+@app.post('/reviews')
+def create():
+  return {}
+
+@app.patch('/reviews/{reviews_id}')
+def updated_review(reviews_id: int):
+  return {'message':f'Review {reviews_id} created successfully'}
+
+@app.delete('/reviews/{reviews_id}')
+def delete_reviews(reviews_id: int):
+  return {'message':f'Review {reviews_id} deleted successfully'}
