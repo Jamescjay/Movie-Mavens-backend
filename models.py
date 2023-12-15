34cfdf831d09fb522ae2827e4c3873216ceeda55
James <james.ekasiba@student.moringaschool.com>
2023-12-15 16:43:04 +0300

    creste movies & users table

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Text, VARCHAR, Integer, DateTime
+from sqlalchemy import Column, Text, VARCHAR, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
@@ -11,3 +12,25 @@ class Review(Base):
   review = Column(VARCHAR, nullable=False)
   date_posted = Column(DateTime(), nullable=False)
   ratings = Column(Integer(), nullable=False)
+
+  movie_id = Column(Integer(), ForeignKey("movies.id"))
+  user_id = Column(Integer(), ForeignKey("users.id"))
+
+
+class User(Base):
+  __tablename__ = "users"
+
+  id = Column(Integer(), primary_key=True)
+  name =Column(Text(), nullable=False)
+
+  reviews = relationship("Review", backref="user")
+
+
+class Movies(Base):
+  __tablename__ = "movies"
+
+  id = Column(Integer(), primary_key=True)
+  title = Column(Integer(), nullable=False)
+  image = Column(VARCHAR, nullable=False)
+
+  audiences = relationship("Review", backref="movie")
--- schemas.py
+++ schemas.py
@@ -2,7 +2,17 @@ from pydantic import BaseModel
 
 
 class ReviewSchema(BaseModel):
+  movie_id: int
+  user_id: int
   name: str
   review: str
   date_posted: str
   ratings: int
+
+class UserSchema(BaseModel):
+  name: str
+
+class MovieSchema(BaseModel):
+  title: str
+  image: str
+  
